c0320a3e96a9ca3d5305b249c08454df73319564
Jacob Sanford <jsanford@unb.ca>
2023-03-17 11:21:37 -0300

    Use twig logic instead of PHP

--- src/Robo/Plugin/Commands/DockworkerInventoryPageCommands.php
+++ src/Robo/Plugin/Commands/DockworkerInventoryPageCommands.php
@@ -57,108 +57,10 @@ class DockworkerInventoryPageCommands extends DockworkerAdminCommands
           '192',
           $this->renderTopicSiteInventoryPageBody(
             'Dockworker',
-            'This is a list of all the Dockworker repositories, grouped by topic.',
+            'This is a list of all the Dockworker repositories, grouped by GitHub topic.',
             $this->getTopicGroupedRepositories()
           )
         );
-    }
-
-    /**
-     * Constructs a list of GitHub repositories grouped by their topics.
-     *
-     * @return array
-     *   An array of GitHub repository objects, keyed by topic.
-     */
-    protected function getTopicGroupedRepositories(): array
-    {
-      $topic_repos = [];
-      foreach ($this->githubRepositories as $repository) {
-        foreach ($repository['topics'] as $topic) {
-          if (!isset($topic_repos[$topic])) {
-              $topic_repos[$topic] = [];
-          }
-          $topic_repos[$topic][] = $repository;
-        }
-      }
-      return $topic_repos;
-    }
-
-    /**
-     * Renders a topic-grouped repository inventory page body.
-     *
-     * @TODO The iteration and output formatting in the called functions should
-     *    be shifted to twig. These functions should be format-agnostic.
-     *
-     * @param string $title
-     *   The title of the page.
-     * @param string $description
-     *   The description of the page.
-     * @param array $repositories
-     *   The repositories to include in the page.
-     *
-     * @return string
-     *   The Markdown for the page.
-     *
-     * @throws \Twig\Error\LoaderError
-     * @throws \Twig\Error\RuntimeError
-     * @throws \Twig\Error\SyntaxError
-     */
-    protected function renderTopicSiteInventoryPageBody(
-        string $title,
-        string $description,
-        array $repositories
-    ): string {
-        return $this->renderTwig(
-            'inventory-page.md.twig',
-            [__DIR__ . '/../../../../data/twig/inventory'],
-            [
-                'title' => $title,
-                'description' => $description,
-                'repo_list' => $this->renderTopicRepositoryList($repositories),
-            ]
-        );
-    }
-
-    /**
-     * Renders a repository list for a topic-grouped inventory page.
-     *
-     * @param array $repositories
-     *   The repositories to include in the list.
-     *
-     * @return string
-     *   The generated markdown for the lists.
-     */
-    protected function renderTopicRepositoryList(
-        array $repositories
-    ): string {
-        $markdown = '';
-        foreach ($repositories as $repository_topic => $repository_list) {
-            $markdown .= "## $repository_topic\n";
-            $markdown .= $this->renderRepositoryList(
-                $repository_list
-            );
-            $markdown .= "\n";
-        }
-        return $markdown;
-    }
-
-    /**
-     * Renders a list of repositories as a Markdown list.
-     *
-     * @param $repositories
-     *   The repositories to render.
-     *
-     * @return string
-     *   The rendered Markdown list.
-     */
-    protected function renderRepositoryList($repositories): string
-    {
-        $markdown = "\n";
-        foreach ($repositories as $repository) {
-            $markdown .= "* [{$repository['name']}]({$repository['html_url']}): {$repository['description']}\n";
-        }
-        $markdown .= "\n";
-        return $markdown;
     }
 
     /**
@@ -174,4 +76,54 @@ class DockworkerInventoryPageCommands extends DockworkerAdminCommands
             'dockworker-admin'
         );
     }
+
+    /**
+     * Renders a topic-grouped repository inventory page body.
+     *
+     * @param string $title
+     *   The title of the page.
+     * @param string $description
+     *   The description of the page.
+     *
+     * @return string
+     *   The Markdown for the page.
+     *
+     * @throws \Twig\Error\LoaderError
+     * @throws \Twig\Error\RuntimeError
+     * @throws \Twig\Error\SyntaxError
+     */
+    protected function renderTopicSiteInventoryPageBody(
+        string $title,
+        string $description
+    ): string {
+        return $this->renderTwig(
+            'inventory-page.md.twig',
+            [__DIR__ . '/../../../../data/twig/inventory'],
+            [
+                'title' => $title,
+                'description' => $description,
+                'repositories' => $this->getTopicGroupedRepositories(),
+            ]
+        );
+    }
+
+  /**
+   * Constructs an associative array of GitHub repositories grouped by topic.
+   *
+   * @return array
+   *   An array of GitHub repository objects, keyed by topic.
+   */
+  protected function getTopicGroupedRepositories(): array
+  {
+    $topic_repos = [];
+    foreach ($this->githubRepositories as $repository) {
+      foreach ($repository['topics'] as $topic) {
+        if (!isset($topic_repos[$topic])) {
+          $topic_repos[$topic] = [];
+        }
+        $topic_repos[$topic][] = $repository;
+      }
+    }
+    return $topic_repos;
+  }
 }
